c70a29c34d7e9e3338fcc7612f8b0f1f2431e3d0
Julia Collins <collinsj@nsidc.org>
2019-07-05 15:00:56 -0600

    Show bundle output in provisioning output; remove unused options from managed-schema files.

--- puppet/site.pp
+++ puppet/site.pp
@@ -92,6 +92,7 @@ unless $environment == 'ci' {
     path => ['/usr/local/bin', '/usr/bin', '/bin'],
     user => 'vagrant',
     group => 'vagrant',
+    logoutput => true,
     require => [ Package['bundler'] ]
   }
 
